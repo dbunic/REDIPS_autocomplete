530d3186ef66febf99b9ab3e6037e688b58c8ed8
dbunic <darko.bunic@gmail.com>
2011-09-05 15:01:16 +0200

    Added readme.md

---
+++ readme.md
@@ -0,0 +1,20 @@
+REDIPS.autocomplete 1.1.0
+============
+
+## What's REDIPS.autocomplete?
+
+REDIPS.autocomplete is a simple JavaScript autocomplete popup. It shows options for typed
+text in input box. With IFRAME and SELECT and without AJAX it should work in any browser.
+
+## Features
+
+* simple
+* uses IFRAME and SELECT multiple
+* AJAX is not used
+
+## Demos
+
+Demo page contains several examples of REDIPS.dialog box. 
+
+* [http://www.redips.net/javascript/autocomplete/](http://www.redips.net/javascript/autocomplete/)
+
--- redips-autocomplete.php
+++ redips-autocomplete.php
@@ -6,12 +6,15 @@ onkeydown  = "javascript:top.keydown(this,event)"
 onblur     = "javascript:top.hidePopup()"
 style      = "width:100%; height:100%">
 
-<?  // create options for select like <option>answer1</option> <option>answer2</option> ...
-		$query = $_REQUEST['query']; // query
-		$fname = $_REQUEST['fname']; // field name
-		for ($i=0; $i<rand(3, 10); $i++) print "<option>$fname$query$i$i$i</option>"; ?>
+<?php
+// create options for select like <option>answer1</option> <option>answer2</option> ...
+$query = $_REQUEST['query']; // query
+$fname = $_REQUEST['fname']; // field name
+for ($i = 0; $i < rand(3, 10); $i++) {
+	print "<option>$fname$query$i$i$i</option>"; 
+}
+?>
 
 </select>
 </body>
 </html>
-
